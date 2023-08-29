8bb02bf7c50aabc5914819fa2461d1df6de1e43f
dannycastilloo <dannycastillootiniano@gmail.com>
2023-08-29 14:51:42 -0500

    instruments description changed

--- src/routes/HomeScreen.jsx
+++ src/routes/HomeScreen.jsx
@@ -6,12 +6,12 @@ export const HomeScreen = () => {
     <>
       <Carousel />
       <div className="catalogue-container">
-        <CatalogueCard img="../src/assets/guitar.jpg" name="Guitar" description="Find your guitar right here. Fender, Gibson, PRS, Yamaha, G&L and more!" url="/guitar" />
-        <CatalogueCard img="../src/assets/bass.jpg" name="Bass" description="Find your guitar right here. Fender, Gibson, PRS, Yamaha, G&L and more!" url="/bass" />
-        <CatalogueCard img="../src/assets/drums.jpg" name="Drums" description="Find your guitar right here. Fender, Gibson, PRS, Yamaha, G&L and more!" url="/drums" />
+        <CatalogueCard img="../src/assets/guitar.jpg" name="Guitar" description="From classic acoustic guitars that resonate with soulful tones to electric guitars that ignite the stage. Just here!"  url="/guitar"/>
+        <CatalogueCard img="../src/assets/bass.jpg" name="Bass" description="We have a big variety of great basses with a big and clear sound. Enter here and choose your favorite!" url="/bass" />
+        <CatalogueCard img="../src/assets/drums.jpg" name="Drums" description="Experience the rhythmic heartbeat of music with our premium selection of drums!" url="/drums" />
         <CatalogueCard img="../src/assets/dj.jpg" name="DJ" description="The best options here to get a high quality DJ and remix all of your favorite songs!" url="/dj" />
-        <CatalogueCard img="../src/assets/trumpet.jpg" name="Trumpet" description="Find your guitar right here. Fender, Gibson, PRS, Yamaha, G&L and more!" url="/trumpet" />
-        <CatalogueCard img="../src/assets/violin.jpg" name="Violin" description="Violins are difficult but great sounding instruments, totally worth it!" url="/violin" />
+        <CatalogueCard img="../src/assets/trumpet.jpg" name="Trumpet" description="Unleash the captivating melodies with our trumpets! Find the perfect instrument to elevate your musical journey." url="/trumpet" />
+        <CatalogueCard img="../src/assets/violin.jpg" name="Violin" description="Explore our collection today and let the strings of your imagination come to life!" url="/violin" />
       </div>
     </>
   )
